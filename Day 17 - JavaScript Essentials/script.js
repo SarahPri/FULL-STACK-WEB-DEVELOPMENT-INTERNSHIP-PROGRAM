142e55f842184d4f527fad331c6e1f0303811f8d
SarahPri <princesswangari09@gmail.com>
2024-08-29 07:03:30 +0300

    JavaScript Essentials

---
+++ Day 17 - JavaScript Essentials/script.js
@@ -0,0 +1,15 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+let num4 = 20;
+
+let greeting = "Hello World";
+let isTrue = true;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+console.log(greeting);
+console.log(isTrue);
