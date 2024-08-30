8829f25ccb1c92cd99771f437485ea1776495c86
SarahPri <princesswangari09@gmail.com>
2024-08-30 17:12:37 +0300

    Sorting

---
+++ Day 36 -Sorting/script.js
@@ -0,0 +1,15 @@
+// Question 1: Sort the given array arr = [26, 42, 78, 18, 3] using Bubble sort.
+
+const arr = [26, 42, 78, 18, 3]
+function bubbleSort(array){
+    //Get the length of the array
+    let n = array.length;
+
+    //Outer loop to make sure we go through the array as many times as needed
+    for(let i = 0; i<n-1; i++){
+        //Inner loop to go through the array and compare adjacent items
+        for(let j=0; j<n-1; j++){
+            
+        }
+    }
+}
